6e4f920376af068edca2da152cddeafb5351d1b5
Youssef-Keshk <160512220+Youssef-Keshk@users.noreply.github.com>
2024-10-25 16:30:13 +0300

    Add Class class (holds gym class information)

---
+++ GymMembershipManagementSystem/src/Class.java
@@ -0,0 +1,41 @@
+public class Class {
+    private String classID, className, trainerID;
+    private int duration, availableSeats;
+
+    public Class(String classID, String className, String trainerID, int duration, int availableSeats) {
+        if (!Validator.isValidID(classID))
+            throw new IllegalArgumentException("Invalid Class ID!");
+        if (!Validator.isValidName(className))
+            throw new IllegalArgumentException("Invalid class name!");
+        if (!Validator.isValidID(trainerID))
+            throw new IllegalArgumentException("Invalid trainer ID!");
+        this.classID = classID;
+        this.className = className;
+        this.trainerID = trainerID;
+        this.duration = duration;
+        this.availableSeats = availableSeats;
+    }
+
+    public int getAvailabeSeats () {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats (int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    public String lineRepresentation () {
+        String line = String.join(",",
+        classID, className, trainerID,
+        String.valueOf(duration), String.valueOf(availableSeats));
+
+        return line;
+    }
+
+    public String getSearchKey () {
+        return classID;
+    }
+
+    
+
+}
