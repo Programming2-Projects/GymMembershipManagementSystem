c507e409ca4db9ada6d6ea308d0bc17d50b6bcfa
Youssef-Keshk <160512220+Youssef-Keshk@users.noreply.github.com>
2024-10-25 16:29:29 +0300

    Edit Members class (validations moved to a sperate class)

--- GymMembershipManagementSystem/src/Member.java
+++ GymMembershipManagementSystem/src/Member.java
@@ -4,13 +4,13 @@ public class Member {
     private MembershipType membershipType;
 
     public Member(String memberId, String name, MembershipType membershipType, String email, String phoneNumber, MembershipStatus status) {
-        if (!isValidID(memberId))
+        if (!Validator.isValidID(memberId))
             throw new IllegalArgumentException("Invalid ID number!");
-        if (!isValidName(name))
+        if (!Validator.isValidName(name))
             throw new IllegalArgumentException("Invalid name!");
-        if (!isValidEmail(email))
+        if (!Validator.isValidEmail(email))
             throw new IllegalArgumentException("Invalid email address!");
-        if (!isValidPhoneNumber(phoneNumber))
+        if (!Validator.isValidPhoneNumber(phoneNumber))
             throw new IllegalArgumentException("Invalid phone number!");
 
         this.memberId = memberId;
@@ -32,36 +32,5 @@ public class Member {
     public String getSearchKey () {
         return memberId;
     }
-
-    // Method to validate a five alpha-numeric ID
-    public static boolean isValidID (String ID) {
-        if (ID.matches("^[\\w]{5}$"))
-            return true;
-        return false;
-    }
-
-    // Method to validate name
-    public static boolean isValidName (String name) {
-        if (name.matches("^[a-zA-Z]{2,}(\\s[a-zA-Z]{2,})*$"))
-            return true;
-        return false;
-    } 
-
-    // Method to validate email address
-    public static boolean isValidEmail (String email) {
-        if (email.matches(
-            "^[\\w.-]+@(gmail|yahoo|outlook|hotmail|icloud|mail|yandex)\\.(com|co\\.uk|co\\.in|ru)$"))
-            return true;
-        return false;
-    }
-
-    // Method to validate phone number --> (+20 0123456789 OR +20123456789 OR 0123456789)
-    public static boolean isValidPhoneNumber (String phoneNumber) {
-        if (phoneNumber.matches(
-            "^(\\+20\\s(10|11|12|15)\\d{8}|\\+20(10|11|12|15)\\d{8}|\\0(10|11|12|15)d{8})$"))
-            return true;
-        return false;
-    } 
-
 
 }
