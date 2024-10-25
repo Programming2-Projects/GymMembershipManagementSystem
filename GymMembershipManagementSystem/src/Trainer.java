0df6170940d74704673386b40522b892f4318caa
ZeyadHaridy <es-zyad.ismail2027@alexu.edu.eg>
2024-10-26 00:23:30 +0300

    Edit Trainer class (Add constructor)

--- GymMembershipManagementSystem/src/Trainer.java
+++ GymMembershipManagementSystem/src/Trainer.java
@@ -5,6 +5,23 @@ private String email;
 private String speciality;
 private String phoneNumber;
 
+public Trainer(String trainerId, String name, String email, String speciality, String phoneNumber) {
+    if (!Validator.isValidID(trainerId))
+        throw new IllegalArgumentException("Invalid ID number!");
+    if (!Validator.isValidName(name))
+        throw new IllegalArgumentException("Invalid name!");
+    if (!Validator.isValidEmail(email))
+        throw new IllegalArgumentException("Invalid email address!");
+    if (!Validator.isValidPhoneNumber(phoneNumber))
+        throw new IllegalArgumentException("Invalid phone number!");
+
+    this.trainerId = trainerId;
+    this.name = name;
+    this.email = email;
+    this.speciality = speciality;
+    this.phoneNumber = phoneNumber;
+}
+
 public String lineRepresentation (){
     String line = String.join(",",
     trainerId,name,email,speciality,phoneNumber);
