6aa017cf711ae31f5396c4627511cd6b7f018347
Youssef-Keshk <160512220+Youssef-Keshk@users.noreply.github.com>
2024-10-26 11:22:11 +0300

    Add constructor

--- GymMembershipManagementSystem/src/MemberClassRegistration.java
+++ GymMembershipManagementSystem/src/MemberClassRegistration.java
@@ -3,25 +3,57 @@ import java.time.*;
 public class MemberClassRegistration {
     private String memberID;
     private String classID;
-    private String status;
+    private MembershipStatus status;
     private LocalDate registrationDate;
+
+    public MemberClassRegistration(String memberID, String classID) {
+        if (!Validator.isValidID(memberID))
+            throw new IllegalArgumentException("Invalid Member ID!");
+        if (!Validator.isValidID(classID))
+            throw new IllegalArgumentException("Invalid Class ID!");
+
+        this.memberID = memberID;
+        this.classID = classID;
+        this.status = MembershipStatus.ACTIVE;
+        this.registrationDate = LocalDate.now();
+    }
+
+    public MemberClassRegistration(String memberID, String classID, MembershipStatus status, LocalDate registrationDate) {
+        if (!Validator.isValidID(memberID))
+            throw new IllegalArgumentException("Invalid Member ID!");
+        if (!Validator.isValidID(classID))
+            throw new IllegalArgumentException("Invalid Class ID!");
+
+        this.memberID = memberID;
+        this.classID = classID;
+        this.status = status;
+        this.registrationDate = registrationDate;
+    }
 
     public String getMemberID() {
         return memberID;
     }
+
     public String getClassID() {
         return classID;
     }
+
     public LocalDate getRegistrationDate() {
         return registrationDate;
     }
-    public String getSearchKey(){
-        return memberID+classID;
-    }
-    public String lineRepresentation (){
-        String line = String.join(",", 
-        memberID,classID,registrationDate.toString(),status);
 
+    public String getSearchKey(){
+        return memberID + classID;
+    }
+
+    public void setStatus (MembershipStatus status) {
+        this.status = status;
+    }
+
+    public String lineRepresentation (){
+        String line = String.join(",", memberID, classID, 
+                        registrationDate.toString(),
+                        String.valueOf(status));
         return line;
     }
     
