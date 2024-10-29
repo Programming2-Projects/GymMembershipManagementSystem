35b09b11602bf0740608d771f7aeae1666599d58
Youssef-Keshk <160512220+Youssef-Keshk@users.noreply.github.com>
2024-10-29 12:21:30 +0300

    Override toString()

--- GymMembershipManagementSystem/src/Trainer.java
+++ GymMembershipManagementSystem/src/Trainer.java
@@ -5,30 +5,40 @@ private String email;
 private String speciality;
 private String phoneNumber;
 
-public Trainer(String trainerId, String name, String email, String speciality, String phoneNumber) {
-    if (!Validator.isValidID(trainerId, 'T'))
-        throw new IllegalArgumentException("Invalid ID number!");
-    if (!Validator.isValidName(name))
-        throw new IllegalArgumentException("Invalid name!");
-    if (!Validator.isValidEmail(email))
-        throw new IllegalArgumentException("Invalid email address!");
-    if (!Validator.isValidPhoneNumber(phoneNumber))
-        throw new IllegalArgumentException("Invalid phone number!");
+    public Trainer(String trainerId, String name, String email, String speciality, String phoneNumber) {
+        if (!Validator.isValidID(trainerId, 'T'))
+            throw new IllegalArgumentException("Invalid ID number!");
+        if (!Validator.isValidName(name))
+            throw new IllegalArgumentException("Invalid name!");
+        if (!Validator.isValidEmail(email))
+            throw new IllegalArgumentException("Invalid email address!");
+        if (!Validator.isValidPhoneNumber(phoneNumber))
+            throw new IllegalArgumentException("Invalid phone number!"); 
 
-    this.trainerId = trainerId;
-    this.name = name;
-    this.email = email;
-    this.speciality = speciality;
-    this.phoneNumber = phoneNumber;
+        this.trainerId = trainerId;
+        this.name = name;
+        this.email = email;
+        this.speciality = speciality;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String lineRepresentation (){
+        String line = String.join(",",
+        trainerId, name, email, speciality, phoneNumber);
+        
+        return line;
+    }
+
+    public String getSearchKey () {
+        return trainerId;
+    }
+
+    @Override
+    public String toString() {
+        return "Trainer [trainerId=" + trainerId + ", name=" + name + ", email=" + email + ", speciality=" + speciality
+                + ", phoneNumber=" + phoneNumber + "]";
+    }
+
+
+
 }
-
-public String lineRepresentation (){
-    String line = String.join(",",
-    trainerId, name, email, speciality, phoneNumber);
-    
-    return line;
-}
-
-public String getSearchKey () {
-    return trainerId;
-}}
