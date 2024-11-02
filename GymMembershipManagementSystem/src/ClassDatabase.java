b2d80efb45b83648ef4d1c21af9a7e2e06d57114
Youssef-Keshk <160512220+Youssef-Keshk@users.noreply.github.com>
2024-11-02 08:36:58 +0200

    Updated the databases

--- GymMembershipManagementSystem/src/ClassDatabase.java
+++ GymMembershipManagementSystem/src/ClassDatabase.java
@@ -1,27 +1,9 @@
-import java.io.*;
-import java.util.ArrayList;
-
-public class ClassDatabase extends DataBases<Class>{
-    private String fileName;
-    private ArrayList<Class> records;
+public class ClassDatabase extends DataBase<Class>{
 
     public ClassDatabase (String fileName) {
-        this.fileName = fileName;
-        this.records = new ArrayList<>();
-        readFromFile();
+        super(fileName);
     }
 
-    @Override
-    public void readFromFile () {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(this.fileName))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null)
-                this.records.add(createRecordFrom(line));
-        } catch (IOException e) {
-            e.getStackTrace();
-        }
-    }
-    
     @Override
     public Class createRecordFrom (String line) {
         if (line == null || line.isEmpty())
@@ -37,55 +19,6 @@ public class ClassDatabase extends DataBases<Class>{
         int duration = Integer.parseInt(data[3]);
         int availableSeats = Integer.parseInt(data[4]);
         return new Class(classID, className, trainerID, duration, availableSeats);
-    }
-
-    @Override
-    public ArrayList<Class> returnAllRecords () {
-        return records;
-    }
-
-    @Override
-    public boolean contains (String key) {
-        for (Class record:records)
-            if (record.getSearchKey().equals(key))
-                return true;
-        return false;
-    }
-
-    @Override
-    public Class getRecord (String key) {
-        for (Class record:records)
-            if (record.getSearchKey().equals(key))
-                return record;
-        return null;
-    }
-
-    @Override
-    public boolean insertRecord (Class record) {
-        if (contains(record.getSearchKey()))
-            return false;
-        this.records.add(record);
-        return true;
-    }
-
-    @Override
-    public boolean deleteRecord (Class record) {
-        return this.records.remove(record); 
-    }
-
-    @Override
-    public boolean saveToFile () {
-        try (PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(this.fileName)))) {
-            String line;
-            for (Class record:records) {
-                line = record.lineRepresentation();
-                printWriter.println(line);
-            }
-            return true;
-        } catch (IOException e) {
-            e.getStackTrace();
-        }
-        return false;
     }
 
 }
--- GymMembershipManagementSystem/src/TrainerDatabase.java
+++ GymMembershipManagementSystem/src/TrainerDatabase.java
@@ -1,25 +1,7 @@
-import java.io.*;
-import java.util.ArrayList;
-
-public class TrainerDatabase extends DataBases<Trainer>{
-    private ArrayList<Trainer> records;
-    private String fileName;
-
+public class TrainerDatabase extends DataBase<Trainer>{
+    
     public TrainerDatabase (String fileName) {
-        this.fileName = fileName;
-        this.records = new ArrayList<>();
-        readFromFile();
-    }
-
-    @Override
-    public void readFromFile () {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(this.fileName))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null)
-                this.records.add(createRecordFrom(line));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        super(fileName);
     }
 
     @Override
@@ -38,55 +20,5 @@ public class TrainerDatabase extends DataBases<Trainer>{
         String phoneNumber = data[4];
         return new Trainer(trainerId, name, email, speciality, phoneNumber);
     }
-
-    @Override
-    public ArrayList<Trainer> returnAllRecords () {
-        return records;
-    }
-
-    @Override
-    public boolean contains (String key) {
-        for (Trainer record:records)
-            if (record.getSearchKey().equals(key))
-                return true;
-        return false;
-    }
-
-    @Override
-    public Trainer getRecord (String key) {
-        for (Trainer trainer:records)
-            if (trainer.getSearchKey().equals(key))
-                return trainer;
-        return null;
-    }
-
-    @Override
-    public boolean insertRecord (Trainer record) {
-        if (contains(record.getSearchKey()))
-            return false;
-        this.records.add(record);
-        return true;
-    }
-
-    @Override
-    public boolean deleteRecord (Trainer record) {
-        return this.records.remove(record); 
-    }
-
-    @Override
-    public boolean saveToFile () {
-        try (PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(this.fileName)))) {
-            String line;
-            for (Trainer trainer:records) {
-                line = trainer.lineRepresentation();
-                printWriter.println(line);
-            }
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-    
     
 }
